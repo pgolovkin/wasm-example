fb3895ea90efe051f9d0940d3595c719db0bec76
pgolovkin <golovkin.pl@gmail.com>
2022-10-07 20:14:53 +0300

    Added struct and impl example

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,21 @@ use wasm_bindgen::prelude::*;
 pub fn greet(name: &str) -> String{
     format!("Hello, {}!", name)
 }
+
+#[wasm_bindgen]
+pub struct Person {
+    age: u32,
+    name: String,
+}
+
+#[wasm_bindgen]
+impl Person {
+    #[wasm_bindgen(constructor)]
+    pub fn new(age: u32, name: String) -> Self {
+        Self { age, name }
+    }
+
+    pub fn info(&self) -> String {
+        format!("{} age is {}", self.name, self.age)
+    }
+}
